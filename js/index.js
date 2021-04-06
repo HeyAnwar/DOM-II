0f61b057e0f30488cdd5c329919f874fc42cd544
Anwar <AnwarAldogmi@gmail.com>
2021-04-06 16:24:28 -0700

    added 3 functions

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
-// Your code goes here
+// const links = document.querySelectorAll('.nav');
+// links.addEventListener('mouseenter', function(event){
+//     event.target.style.color = 'purple';
+// })
+//#1
+const funBus = document.querySelector('h1.logo-heading')
+funBus.addEventListener('mouseenter', function(event) {
+    event.target.style.color = 'green';
+  });
+//#2
+const welcomeBus = document.querySelector('.intro')
+welcomeBus.addEventListener('mouseenter', function(event){
+    event.target.style.background = 'purple'
+})
+//#3
+const textContent = document.querySelector('.text-content')
+textContent.addEventListener('dblclick', function (event){
+    event.target.style.background = 'blue'
+  });
